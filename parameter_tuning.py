a9a28c6fb490dd9418036fe011b7a3765ddd9995
ChewKinWhye <Ckinwhye@hotmail.com>
2020-08-31 17:54:34 +0800

    Grid search

--- parameter_tuning.py
+++ parameter_tuning.py
@@ -10,39 +10,37 @@ from random import uniform
 if __name__ == "__main__":
     args = parse_args()
     x_train, y_train, x_test, y_test, _, _ = load_dna_data_vae(args.data_size, args.data_path)
-    trials = 1000
     best_accuracy = float("-inf")
-    # 479
     for mean_scale in range(1, 10):
-        # Train VAE
-        mean_scale, std_scale = uniform(1, 10.0), uniform(1, 10.0)
-        len_scale, signal_scale = uniform(1, 10.0), uniform(1, 10.0)
-        x_train_scaled = np.concatenate((x_train[:, 0:68], x_train[:, 68:85]*mean_scale, x_train[:, 85:102]*std_scale,
-                                         x_train[:, 102:119]*len_scale, x_train[:, 119:]*signal_scale), axis=1)
-        x_test_scaled = np.concatenate((x_test[:, 0:68], x_test[:, 68:85] * mean_scale, x_test[:, 85:102] * std_scale,
-                                        x_test[:, 102:119] * len_scale, x_test[:, 119:] * signal_scale), axis=1)
+        for std_scale in range(1, 10):
+            for len_scale in range(1, 10):
+                for signal_scale in range(5, 15):
+                    x_train_scaled = np.concatenate((x_train[:, 0:68], x_train[:, 68:85]*mean_scale, x_train[:, 85:102]*std_scale,
+                                                     x_train[:, 102:119]*len_scale, x_train[:, 119:]*signal_scale), axis=1)
+                    x_test_scaled = np.concatenate((x_test[:, 0:68], x_test[:, 68:85] * mean_scale, x_test[:, 85:102] * std_scale,
+                                                    x_test[:, 102:119] * len_scale, x_test[:, 119:] * signal_scale), axis=1)
 
-        vae = VaeDNA(args.latent_dim, args.rc_loss_scale)
-        vae.compile(optimizer=keras.optimizers.Adam())
-        vae.fit(x_train_scaled, epochs=args.vae_epochs, batch_size=args.vae_batch_size, verbose=0)
+                    vae = VaeDNA(args.latent_dim, args.rc_loss_scale)
+                    vae.compile(optimizer=keras.optimizers.Adam())
+                    vae.fit(x_train_scaled, epochs=args.vae_epochs, batch_size=args.vae_batch_size, verbose=0)
 
-        # Visualize cluster
-        # Train predictor
-        predictor = load_vae_predictor(args.latent_dim)
-        # Prepared predictor training input
-        predictor_size = int(len(x_train)/10)
-        x_train_mean, x_train_sd, _ = vae.encoder.predict(x_train_scaled[0:predictor_size])
-        x_train_predictor = np.concatenate((x_train_mean, x_train_sd), axis=1)
-        predictor.fit(x_train_predictor, y_train[0:predictor_size], epochs=args.predictor_epochs,
-                      batch_size=args.predictor_batch_size, verbose=0)
+                    # Visualize cluster
+                    # Train predictor
+                    predictor = load_vae_predictor(args.latent_dim)
+                    # Prepared predictor training input
+                    predictor_size = int(len(x_train)/10)
+                    x_train_mean, x_train_sd, _ = vae.encoder.predict(x_train_scaled[0:predictor_size])
+                    x_train_predictor = np.concatenate((x_train_mean, x_train_sd), axis=1)
+                    predictor.fit(x_train_predictor, y_train[0:predictor_size], epochs=args.predictor_epochs,
+                                  batch_size=args.predictor_batch_size, verbose=0)
 
-        # Test model
-        x_test_mean, x_test_sd, _ = vae.encoder.predict(x_test_scaled)
-        x_test_predictor = np.concatenate((x_test_mean, x_test_sd), axis=1)
-        predictions = predictor.predict(x_test_predictor)
-        test_results = compute_metrics_standardized(predictions, y_test)
-        if test_results[0] > best_accuracy:
-            best_accuracy = test_results[0]
-            print("---------------")
-            print(mean_scale, std_scale, len_scale, signal_scale)
-            print(test_results[0])
+                    # Test model
+                    x_test_mean, x_test_sd, _ = vae.encoder.predict(x_test_scaled)
+                    x_test_predictor = np.concatenate((x_test_mean, x_test_sd), axis=1)
+                    predictions = predictor.predict(x_test_predictor)
+                    test_results = compute_metrics_standardized(predictions, y_test)
+                    if test_results[0] > best_accuracy:
+                        best_accuracy = test_results[0]
+                        print("---------------")
+                        print(mean_scale, std_scale, len_scale, signal_scale)
+                        print(test_results[0])
