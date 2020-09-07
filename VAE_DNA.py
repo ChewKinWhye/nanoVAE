fe9d3a3133d33ccd57191d918e7bbe61861baf32
ChewKinWhye <Ckinwhye@hotmail.com>
2020-09-07 16:24:16 +0800

    New VAE model

--- VAE_DNA.py
+++ VAE_DNA.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils.data import load_dna_data_vae, load_multiple_reads_data
 from utils.arguments import parse_args
 from utils.evaluate import compute_metrics_standardized, plot_label_clusters, print_results
-from utils.model import VaeDNA, load_vae_predictor
+from utils.model import VaeDNA, load_vae_predictor, load_vae_dna_model
 from utils.save import save_results
 from tensorflow.keras.callbacks import EarlyStopping
 
@@ -14,7 +14,8 @@ if __name__ == "__main__":
 
     x_train, y_train, x_test, y_test, x_val, y_val = load_dna_data_vae(args.data_size, args.data_path)
     # Train VAE
-    vae = VaeDNA(args.latent_dim, args.rc_loss_scale)
+    # vae = VaeDNA(args.latent_dim, args.rc_loss_scale)
+    encoder, decoder, vae = load_vae_dna_model()
     vae.compile(optimizer=keras.optimizers.Adam())
     vae.fit(x_train, epochs=args.vae_epochs, batch_size=args.vae_batch_size, verbose=2)
 
--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,52 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras import backend as K
+from tensorflow.keras.losses import mse
+
+def sampling(args):
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+
+def load_vae_dna_model(latent_dim, rc_loss_scale):
+    # Build encoder
+    encoder_inputs = keras.Input(shape=(479,))
+    x = layers.Dense(256, activation="relu")(encoder_inputs)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dense(64, activation="relu")(x)
+    x = layers.Dense(32, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = keras.Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    # Build decoder
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = layers.Dense(32, activation="relu")(latent_inputs)
+    x = layers.Dense(64, activation="relu")(x)
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dense(479, activation="linear")(x)
+    decoder_outputs = layers.Reshape((479,))(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    outputs = decoder(encoder(encoder_inputs)[2])
+    vae = keras.Model(encoder_inputs, outputs, name='vae_mlp')
+
+    reconstruction_loss = mse(encoder_inputs, outputs)
+    reconstruction_loss *= rc_loss_scale
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    return encoder, decoder, vae
 
 
 class Sampling(layers.Layer):
