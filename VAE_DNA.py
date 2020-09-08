3a2b1091717e5c2006daa8e00503a38ac48c0443
ChewKinWhye <e0200920@u.nus.edu>
2020-09-08 05:45:04 +0000

    tensorboard and early stopping

--- VAE_DNA.py
+++ VAE_DNA.py
@@ -6,18 +6,21 @@ from utils.arguments import parse_args
 from utils.evaluate import compute_metrics_standardized, plot_label_clusters, print_results
 from utils.model import VaeDNA, load_vae_predictor, load_vae_dna_model
 from utils.save import save_results
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+import time
 
 
 if __name__ == "__main__":
     args = parse_args()
-
+    supervised_size = 10000
+    NAME = f"VAE_DNA-{int(time.time())}"
     x_train, y_train, x_test, y_test, x_val, y_val = load_dna_data_vae(args.data_size, args.data_path)
     # Train VAE
     # vae = VaeDNA(args.latent_dim, args.rc_loss_scale)
     encoder, decoder, vae = load_vae_dna_model(args.latent_dim, args.rc_loss_scale)
-    vae.compile(optimizer=keras.optimizers.Adam())
-    vae.fit(x_train, epochs=args.vae_epochs, batch_size=args.vae_batch_size, verbose=2)
+    es = EarlyStopping(monitor='val_loss', mode='min', patience=20)
+    tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
+    vae.fit(x_train[0:int(len(x_train)*0.8)], validation_data=(x_train[int(len(x_train)*0.8):], None), epochs=args.vae_epochs, batch_size=args.vae_batch_size, verbose=2, callbacks=[es, tensorboard])
 
     # Visualize cluster
     encoding_cluster_plt = plot_label_clusters(encoder, x_train, y_train)
@@ -25,11 +28,11 @@ if __name__ == "__main__":
     # Train predictor
     predictor = load_vae_predictor(args.latent_dim)
     # Prepared predictor training input
-    predictor_size = int(len(x_train)/10)
-    x_train_mean, x_train_sd, _ = encoder.predict(x_train[0:predictor_size])
+    x_train_mean, x_train_sd, _ = encoder.predict(x_train[0:supervised_size])
     x_train = np.concatenate((x_train_mean, x_train_sd), axis=1)
-    predictor.fit(x_train, y_train[0:predictor_size], epochs=args.predictor_epochs,
-                  batch_size=args.predictor_batch_size)
+    es = EarlyStopping(monitor='val_loss', mode='min', patience=10)
+    predictor.fit(x_train, y_train, epochs=args.predictor_epochs, validation_split=0.2,
+                  batch_size=args.predictor_batch_size, callbacks=[es])
 
     # Test model
     x_test_mean, x_test_sd, _ = encoder.predict(x_test)
